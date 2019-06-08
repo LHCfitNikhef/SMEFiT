02f842f7bc67003d7d4400ba55fcb164ca75bc7a
samuelvanbeek <samuelvanbeek@gmail.com>
2019-06-08 11:04:50 +0200

    code ready for revision, input of code in code_input.py

--- SMEFiT_rw_unw.py
+++ SMEFiT_rw_unw.py
@@ -1,5 +1,4 @@
 '''
-
 ##======================================##
 || Code for reweighting and unweighting ||
 ##======================================##
@@ -12,7 +11,7 @@ experimental data on a number of theoretical predictions.
 +--------------+
 
 In order to run the code, one needs to have the data of an ensemble of Wilson
-coefficients sets with corresponding chi2 data. The Wilson coefficients should
+coefficients with corresponding chi2 data. The Wilson coefficients should
 have been used as free parameters in a SMEFT theory prediction for the new data
 of which the chi2 is determined. In the folder "rw_input_data/", sets of Wilson
 coefficients and chi2 data are available.
@@ -31,7 +30,7 @@ Needed Python packages:
 The code gives the following output:
   * Plot of the 2sigma bounds of the prior/posterior/reweighted/unweighted set,
     together with a plot of the amount of error reduction and the KS statistics
-  * Coefficient distributions for well-constrained operators
+  * Coefficient distributions for constrained operators satisfying the criteria
   * Text file with the coefficients of the unweighted set for all operators
     considered
 
@@ -57,9 +56,11 @@ The code gives the following output:
 - Take a number copies of the predictions in the prior ensemble according to the
   corresponding integer weights to obtain the unweighted set
 - Calculate the Kolmogorov-Smirnov statistic between unweighted and prior set
-- Plotting section
-
+- Load in the posterior for validation
+- Produce results
 '''
+
+################################################################################
 
 '''
 +----------------------------+
@@ -90,7 +91,7 @@ print('\n* Prior: \n ', prior_data)
 print('\n* Posterior:\n ', poster_data)
 print('\n* Number of replicas :\n ', n_reps)
 print('\n* Minimal KS :\n ', ks_level)
-print('\n* Minimal sigma reduction :\n ', reduction_level)
+print('\n* Minimal error reduction :\n ', reduction_level)
 
 # Load in the prior coefficient distributions and the chi2 data
 prior_coeffs_list = []
@@ -142,7 +143,7 @@ chi2_all_reps = np.asarray(chi2_array[:, 0], dtype=float)
 n_datapoints = np.asarray(chi2_array[:, 1], dtype=int)
 chi2_norm_all_reps = np.asarray(chi2_array[:, 2], dtype=float)
 
-print('\n* 5 lowest normalized chi2s :\n ', np.sort(chi2_norm_all_reps)[0:5])
+print('\n* 5 lowest normalized chi2s :\n ', np.round(np.sort(chi2_norm_all_reps)[0:5],2))
 
 # Calculate the weights
 def calculate_weights(scaling_factor) :
@@ -154,7 +155,7 @@ def calculate_weights(scaling_factor) :
     return nnpdf_weights
 
 nnpdf_weights = calculate_weights(scaling_factor=1)
-print('\n* 5 highest weights :\n ', np.sort(nnpdf_weights)[-5:])
+print('\n* 5 highest weights :\n ', np.round(np.sort(nnpdf_weights)[-5:],2))
 
 # Replace very small weights to prevent infinities/divide-by-0's
 zero_weights = np.asarray(np.where(nnpdf_weights < 1.0e-300))[0]
@@ -163,11 +164,11 @@ print('\n* ' + str(len(zero_weights)) + ' weights below 1.0e-300 were replaced b
 
 # Check that normalization is satisfied
 assert np.round(np.sum(nnpdf_weights)) == n_reps, 'sum of weights should equal number of replicas'
-print('\n* sum of weights :\n ', np.sum(nnpdf_weights))
+print('\n* Sum of weights :\n ', np.sum(nnpdf_weights))
 
 # Determine the Shannon entropy (number of effective replicas)
 n_eff = np.exp(1/n_reps * np.sum(nnpdf_weights * np.log(n_reps/nnpdf_weights)))
-print('\n* N_eff after reweighting:\n ', n_eff)
+print('\n* N_eff after reweighting:\n ', np.round(n_eff,2))
 
 # Obtain the reweighted distributions and standard deviations for the coefficients
 rw_coeffs = np.transpose(np.multiply(nnpdf_weights, np.transpose(prior_coeffs)))
@@ -220,7 +221,7 @@ unw_weights = np.sum(np.asarray(unw_weights_list, dtype=int), axis=1)
 ## Check that normalization is satisfied
 assert np.round(np.sum(unw_weights)) == np.floor(unw_n_reps), \
 'integer weights after unweighting do not satisfy normalization'
-print('\n* sum of integer weights after unweighting :\n ', np.sum(unw_weights))
+print('\n* Sum of integer weights after unweighting :\n ', np.sum(unw_weights))
 
 ## Obtain the unweighted distributions for the coefficients
 surv_rep_nums = np.where(unw_weights != 0)[0]
@@ -299,64 +300,6 @@ constr_unw_coeffs = np.take(np.transpose(unw_coeffs), constr_op_nums, axis=0)
 constr_ks_stats = np.take(ks_stats, constr_op_nums)
 
 '''
-+--------------+
-| Chi2 profile |
-+--------------+
-'''
-
-
-
-# Calculate the rescaled weights
-alphas = np.linspace(0.1, 5, 100)
-# alphas = [1.0,1.5]
-p_alphas = []
-print('alpha p_alpha')
-for alpha in alphas :
-    nnpdf_weights_alpha = calculate_weights(scaling_factor=alpha)
-
-    # Solve the integral
-    beta_list = np.linspace(-3,5, 1000)
-    nnpdf_weights_beta_list = []
-    for beta in beta_list :
-        nnpdf_weights_beta = calculate_weights(scaling_factor=np.exp(beta))
-        nnpdf_weights_beta_list.append(nnpdf_weights_beta)
-    nnpdf_weights_beta_array = np.asarray(nnpdf_weights_beta_list)
-
-    integral_list = []
-    for k in np.arange(n_reps) :
-        integral = intg.simps(nnpdf_weights_beta_array[:,k], beta_list)
-        integral_list.append(integral)
-    integral_array = np.asarray(integral_list)
-
-    # Replace very small integrals with 1e-300 to prevent divide-by-zero's
-    zero_integrals = np.asarray(np.where(integral_array < 1.0e-300))[0]
-    np.put(integral_array, zero_integrals, 1e-300)
-
-    p_alpha = 1/n_reps * np.sum(nnpdf_weights_alpha / (alpha * integral_array))
-    print(np.round(alpha,2),'\t' ,np.round(p_alpha,2))
-    p_alphas.append(p_alpha)
-
-p_alphas = np.asarray(p_alphas)
-
-fig, axes = plt.subplots(1,2)
-
-ax1, ax2 = axes
-
-ax1.plot(alphas, p_alphas, ':')
-ax1.set_yscale('log')
-ax1.set_xlabel('$\\alpha$')
-ax1.set_ylabel('$P(\\alpha)$')
-
-
-ax2.plot(alphas, p_alphas, ':')
-ax2.set_xlabel('$\\alpha$')
-ax2.set_ylabel('$P(\\alpha)$')
-
-fig.savefig('p_alpha.pdf', dpi=1000, bbox_inches='tight')
-
-
-
-'''
 +--------------------------+
 | Print table in  terminal |
 +--------------------------+
@@ -365,13 +308,15 @@ fig.savefig('p_alpha.pdf', dpi=1000, bbox_inches='tight')
 def print_constrained_operator_table() :
 
     print('\n')
-    print('\t\t\t ** Table of constrained operators **')
-    print('\t\t\t    ------------------------------   ')
+    print('\t\t\t     +--------------------------------+     ')
+    print('\t\t\t --- | Table of constrained operators | --- ')
+    print('\t\t\t     +--------------------------------+ \n  ')
+
     headers = ['operator',
-               'prior std dev',
-               'poster st devs',
-               'rw std dev',
-               'unw std dev',
+               'prior st dev',
+               'poster st dev',
+               'rw st dev',
+               'unw st dev',
                'KS stat',
               ]
 
@@ -387,7 +332,6 @@ def print_constrained_operator_table() :
 
     return None
 
-
 print_constrained_operator_table()
 
 '''
@@ -397,6 +341,7 @@ print_constrained_operator_table()
 '''
 
 def save_unw_set() :
+
     # format the list of operator names
     names_list = []
     for name in np.arange(len(op_names)) :
@@ -405,9 +350,13 @@ def save_unw_set() :
     names_header = ''.join((np.asarray(names_list, dtype=str)))
 
     # save unweighted set in text file
-    np.savetxt('unw_coeffs.txt', unw_coeffs, fmt='%10.5f', header=names_header, comments='')
+    np.savetxt('rw_output/unw_coeffs.txt', unw_coeffs, fmt='%10.5f', header=names_header, comments='')
 
-    return no_single_top
+    print('\n* Unweighted set saved in text file')
+
+    return None
+
+save_unw_set()
 
 '''
 +------------------+
@@ -415,124 +364,126 @@ def save_unw_set() :
 +------------------+
 '''
 
-# Define colors
-color_purple        = (0.85, 0.4, 0.55)
-color_turqoise      = (0.20, 0.70, 0.60)
-color_dark_turqoise = (0.20, 0.50, 0.50)
-color_yellow        = (0.95, 0.75, 0.20)
-color_green         = (0.10, 0.60, 0.40)
-color_green_line    = (0.05, 0.40, 0.10)
-color_light_grey    = (0.85, 0.85, 0.85)
-color_dark_grey     = (0.00, 0.00, 0.00, 0.60)
-color_white         = (0.95,0.95,0.95)
+if inp.produce_plots == 'on' :
 
-# General settings for plotting
-plt.rc('axes', axisbelow=True)
-plt.rc('axes', edgecolor=color_dark_grey)
-plt.rcParams['xtick.color'] = color_dark_grey
-mpl.rcParams['ytick.color'] = color_dark_grey
+    # Make folder for the output
+    if not os.path.exists('rw_output/') :
+        os.makedirs('rw_output/')
 
-# Make folder for the plots
-if not os.path.exists('plots/') :
-    os.makedirs('plots/')
+    # Define colors
+    color_purple        = (0.85, 0.4, 0.55)
+    color_turqoise      = (0.20, 0.70, 0.60)
+    color_dark_turqoise = (0.20, 0.50, 0.50)
+    color_yellow        = (0.95, 0.75, 0.20)
+    color_green         = (0.10, 0.60, 0.40)
+    color_green_line    = (0.05, 0.40, 0.10)
+    color_light_grey    = (0.85, 0.85, 0.85)
+    color_dark_grey     = (0.00, 0.00, 0.00, 0.60)
+    color_white         = (0.95,0.95,0.95)
 
-def plot_two_sigma_bounds() :
+    # General settings for plotting
+    plt.rc('axes', axisbelow=True)
+    plt.rc('axes', edgecolor=color_dark_grey)
+    plt.rcParams['xtick.color'] = color_dark_grey
+    mpl.rcParams['ytick.color'] = color_dark_grey
 
-    print('\n* Plotting two sigma bounds ...')
+    def plot_two_sigma_bounds() :
 
-    # make figure opbject
-    fig, axes = plt.subplots(3,1, sharex=True)
-    ax1, ax2, ax3 = axes
-    fig.tight_layout()
-    plt.subplots_adjust(hspace=0.1)
-
-    # needed for bar plots
-    bar_width = 0.15
-    bar_shift = 0.03
-    op_list = np.arange(1, len(op_names)+1)
-
-    # two sigma bounds prior/poster/rw/unw
-    ax1.bar(op_list, 2.0*prior_st_devs, label='prior', width=4.1*bar_width, color='black', align='center', alpha=0.7)
-    ax1.bar(op_list-bar_width-bar_shift, 2.0*poster_st_devs, label='posterior' , width=bar_width, color=color_purple, align='center' )
-    ax1.bar(op_list, 2.0*rw_st_devs, label='reweighted', width=bar_width, color=color_turqoise, align='center')
-    ax1.bar(op_list+bar_width+bar_shift, 2.0*unw_st_devs, label='unweighted', width=bar_width, color=color_yellow, align='center')
-
-    # layout settings two sigma bounds
-    ax1.grid(True, axis='y', color=color_light_grey)
-    ax1.set_yscale('log')
-    ax1.set_xticks(op_list)
-    ax1.set_ylabel('$2\sigma$ [TeV$^{-2}$]', color=color_dark_grey)
-    legend1 = ax1.legend(loc='best', ncol=2, facecolor=color_white)
-
-    # reduction plot
-    ax2.axhline(y=reduction_level, color=color_green_line, lw=2, alpha=0.6, linestyle='dotted')
-    ax2.bar(op_list, reduction_poster, label='1 - $\\frac{\sigma_{post}}{\sigma_{prior}}$', color=color_purple  , width=1.5*bar_width)
-    ax2.bar(op_list, reduction_rw, label='1 - $\\frac{\sigma^{NNPDF}_{rw}}{\sigma_{prior}}$', color=color_dark_turqoise, width=3.0*bar_width, alpha=0.3)
-
-    # layout reduction plot
-    ax2.grid(True, axis='y', color=color_light_grey)
-    ax2.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-    ax2.set_ylabel('Reduction in $\sigma$', color=color_dark_grey)
-    legend2 = ax2.legend(loc='best', ncol=2, facecolor=color_white)
-
-    # plot KS stats
-    ax3.axhline(y=ks_level, color=color_green_line, lw=2, alpha=0.6, linestyle='dotted')
-    ax3.plot(op_list, ks_stats, 'd', label='$KS$-stat', color=color_dark_turqoise, ms=4, mfc='maroon')
-    ax3.vlines(op_list, 0, ks_stats, color=color_dark_turqoise, lw=1, linestyle='dashed')
-
-    # layout KS stats plot
-    ax3.set_yticks([0.0,0.1,0.2,0.3,0.4,0.5])
-    ax3.grid(True, axis='y', color=color_light_grey)
-    ax3.set_xticklabels(op_names, rotation=90, fontsize=8)
-    ax3.set_ylabel('KS-stat', color=color_dark_grey)
-
-    # save figure
-    fig.savefig('plots/two_sigma_bounds_KS_reduction_' + poster_data + '.pdf', dpi=1000, bbox_inches='tight')
-
-    return None
-
-def plot_distr_constr_ops() :
-
-    print('\n* Plotting coefficient distributions ...')
-
-    # needed for histogram
-    nbins = 30
-    constr_op_list = np.arange(1, len(constr_op_names)+1)
-
-    # loop over the constrained operators
-    for oper in np.arange(len(constr_op_nums)) :
+        print('\n* Plotting two sigma bounds ...')
 
         # make figure opbject
-        fig, ax = plt.subplots()
+        fig, axes = plt.subplots(3,1, sharex=True)
+        ax1, ax2, ax3 = axes
         fig.tight_layout()
+        plt.subplots_adjust(hspace=0.1)
 
-        # make histograms of the coefficients
-        hist_range = [-10, 10]
+        # needed for bar plots
+        bar_width = 0.15
+        bar_shift = 0.03
+        op_list = np.arange(1, len(op_names)+1)
 
-        # prior histogram
-        ax.hist(constr_prior_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='stepfilled', color=color_dark_grey, label='prior', alpha=0.15)
-        ax.hist(constr_prior_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='step', color=color_dark_grey, alpha=0.3)
+        # two sigma bounds prior/poster/rw/unw
+        ax1.bar(op_list, 2.0*prior_st_devs, label='prior', width=4.1*bar_width, color='black', align='center', alpha=0.7)
+        ax1.bar(op_list-bar_width-bar_shift, 2.0*poster_st_devs, label='posterior' , width=bar_width, color=color_purple, align='center' )
+        ax1.bar(op_list, 2.0*rw_st_devs, label='reweighted', width=bar_width, color=color_turqoise, align='center')
+        ax1.bar(op_list+bar_width+bar_shift, 2.0*unw_st_devs, label='unweighted', width=bar_width, color=color_yellow, align='center')
 
-        # posterior histogram
-        ax.hist(constr_poster_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='stepfilled', color=color_purple, label='posterior', alpha=0.4)
+        # layout settings two sigma bounds
+        ax1.grid(True, axis='y', color=color_light_grey)
+        ax1.set_yscale('log')
+        ax1.set_xticks(op_list)
+        ax1.set_ylabel('$2\sigma$ [TeV$^{-2}$]', color=color_dark_grey)
+        legend1 = ax1.legend(loc='best', ncol=2, facecolor=color_white)
 
-        # unweighted histogram
-        ax.hist(constr_unw_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='stepfilled', color=color_turqoise, lw=4, alpha=0.1)
-        ax.hist(constr_unw_coeffs[oper], bins=nbins, density=True, range=hist_range, label='unweighted', histtype='step', lw=2, color=color_turqoise)
+        # reduction plot
+        ax2.axhline(y=reduction_level, color=color_green_line, lw=2, alpha=0.6, linestyle='dotted')
+        ax2.bar(op_list, reduction_poster, label='1 - $\\frac{\sigma_{post}}{\sigma_{prior}}$', color=color_purple  , width=1.5*bar_width, alpha=1.0)
+        ax2.bar(op_list, reduction_rw, label='1 - $\\frac{\sigma^{NNPDF}_{rw}}{\sigma_{prior}}$', color=color_turqoise, width=3.0*bar_width, alpha=0.3)
 
-        # add legend
-        legend = ax.legend(loc='best', title='$' + str(constr_op_names[oper]) + '$', title_fontsize=13, facecolor=color_white)
-        legend._legend_box.align = 'left'
+        # layout reduction plot
+        ax2.grid(True, axis='y', color=color_light_grey)
+        ax2.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
+        ax2.set_ylabel('Reduction in $\sigma$', color=color_dark_grey)
+        legend2 = ax2.legend(loc='best', ncol=2, facecolor=color_white)
 
-        # set x and y labels
-        ax.set_xlabel('$c_{' + str(constr_op_names[oper]) + '}$', color=color_dark_grey, fontsize=12)
-        ax.set_ylabel('probability density', color=color_dark_grey, fontsize=12)
+        # plot KS stats
+        ax3.axhline(y=ks_level, color=color_green_line, lw=2, alpha=0.6, linestyle='dotted')
+        ax3.plot(op_list, ks_stats, 'd', label='$KS$-stat', color=color_dark_turqoise, ms=4, mfc='maroon')
+        ax3.vlines(op_list, 0, ks_stats, color=color_turqoise, lw=1, linestyle='dashed')
+
+        # layout KS stats plot
+        ax3.set_yticks([0.0,0.1,0.2,0.3,0.4,0.5])
+        ax3.grid(True, axis='y', color=color_light_grey)
+        ax3.set_xticklabels(op_names, rotation=90, fontsize=8)
+        ax3.set_ylabel('KS-stat', color=color_dark_grey)
 
         # save figure
-        fig.savefig('plots/distr_' + str(constr_op_names[oper]) + '_' + poster_data + '.pdf', dpi=1000, bbox_inches='tight')
+        fig.savefig('rw_output/two_sigma_bounds_KS_reduction_' + poster_data + '.pdf', dpi=1000, bbox_inches='tight')
 
-    return None
+        return None
 
-# plot_distr_constr_ops()
-# plot_two_sigma_bounds()
-print('\n* Plots are produced and saved')
+    def plot_distr_constr_ops() :
+
+        print('\n* Plotting coefficient distributions ...')
+
+        # needed for histogram
+        nbins = 30
+        constr_op_list = np.arange(1, len(constr_op_names)+1)
+
+        # loop over the constrained operators
+        for oper in np.arange(len(constr_op_nums)) :
+
+            # make figure opbject
+            fig, ax = plt.subplots()
+            fig.tight_layout()
+
+            # make histograms of the coefficients
+            hist_range = [-10, 10]
+
+            # prior histogram
+            ax.hist(constr_prior_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='stepfilled', color=color_dark_grey, label='prior', alpha=0.15)
+            ax.hist(constr_prior_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='step', color=color_dark_grey, alpha=0.3)
+
+            # posterior histogram
+            ax.hist(constr_poster_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='stepfilled', color=color_purple, label='posterior', alpha=0.4)
+
+            # unweighted histogram
+            ax.hist(constr_unw_coeffs[oper], bins=nbins, density=True, range=hist_range, histtype='stepfilled', color=color_turqoise, lw=4, alpha=0.1)
+            ax.hist(constr_unw_coeffs[oper], bins=nbins, density=True, range=hist_range, label='unweighted', histtype='step', lw=2, color=color_turqoise)
+
+            # add legend
+            legend = ax.legend(loc='best', title='$' + str(constr_op_names[oper]) + '$', title_fontsize=13, facecolor=color_white)
+            legend._legend_box.align = 'left'
+
+            # set x and y labels
+            ax.set_xlabel('$c_{' + str(constr_op_names[oper]) + '}$', color=color_dark_grey, fontsize=12)
+            ax.set_ylabel('probability density', color=color_dark_grey, fontsize=12)
+
+            # save figure
+            fig.savefig('rw_output/distr_' + str(constr_op_names[oper]) + '_' + poster_data + '.pdf', dpi=1000, bbox_inches='tight')
+
+        return None
+
+    plot_distr_constr_ops()
+    plot_two_sigma_bounds()
+    print('\n* Plots are produced and saved')
--- code_input.py
+++ code_input.py
@@ -1,16 +1,33 @@
-
 '''
-+----------------------------+
-| Input for reweighting code |
-+----------------------------+
++-------------------------------------+
+| Input settings for reweighting code |
++-------------------------------------+
+
+The settings for the reweighting procedure can be set in this file.
+One needs to choose the following:
+(1) the number of replicas used for reweighting
+(2) the minimal required error reduction
+(3) the minimal required KS-statistic
+(4) decide if plots need to be produced
+(5) dataset for the prior
+(6) dataset for the posterior
+
+Run 'SMEFiT_rw_unw.py' after the options have been set.
 '''
 
-# Set the number of replicas, the KS criterium and required error reduction
-n_reps          = 1000
-ks_level        = 0.3
+# (1) Set the number of replicas
+n_reps          = 10000
+
+# (2) Set the minimal required error reduction
 reduction_level = 0.3
 
-# Choose one of the priors
+# (3) Set the minimal required KS-statistic
+ks_level        = 0.3
+
+# (4) Set 'on' to produce plots
+produce_plots   = 'on'
+
+# (5) Choose a prior from the list
 prior_data_list = [
                    'no_single_top',
                    # 'all_datasets',
@@ -22,13 +39,13 @@ prior_data_list = [
 prior_data = prior_data_list[0]
 
 
-# Choose the posterior for validation
+# (6) Choose a posterior from the list
 poster_data_list = [
                     # 'no_single_top',
                     # 'all_datasets',
-                    '1st_t_channel',
+                    # '1st_t_channel',
                     # 'only_ttbar',
                     # 's_channel',
-                    # 't_channel',
+                    't_channel',
                    ]
 poster_data = poster_data_list[0]
