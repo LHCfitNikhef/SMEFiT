5d83e1f297ff7677b788c393ec0e390680f167cc
giacomomagni <giac.magni@gmail.com>
2021-07-19 12:36:02 +0200

    Adding VBS paper results

--- replicas_to_json.py
+++ replicas_to_json.py
@@ -10,11 +10,18 @@ def convert_to_json(k):
     coeffs = []
     vals = {}
 
+    # VBS paper replicas were with a different convention,
+    # need to add a multiplicative factor
+    rescale_fact = 1.0
+    if "VBS" in k:
+        rescale_fact = 1/0.246 ** 2
+        print("Rescale factor: ", rescale_fact)
+
     counterbar = Bar(r"Reading replica", max=len(os.listdir(k)) - 1)
     for i, filename in enumerate( os.listdir(k)):
-        if filename.startswith("SMEFT_coeffs_") is False or filename.endswith(
+        if filename.startswith("SMEFT_coeffs_") is False or ( filename.endswith(
             "_0.txt"
-        ):
+        ) and "VBS" not in k):
             continue
 
         file = open(f"{k}/{filename}", "r")
@@ -29,7 +36,7 @@ def convert_to_json(k):
             if op not in coeffs:
                 raise UserWarning(f"Fonund a new op in replica, {filename}: {op}")
 
-        temp = dict(zip(coeffs, [float(i) for i in data[1].split()]))
+        temp = dict(zip(coeffs, [rescale_fact * float(i) for i in data[1].split()]))
         for c in coeffs:
             vals[c].append(temp[c])
         file.close()
